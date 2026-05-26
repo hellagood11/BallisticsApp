44cab4b3be16cb9b428e1941422842286d09d379
Ronan <rhella@uccs.edu>
2026-05-25 22:39:40 -0600

    Restructured some of the class, added basic html page and css

--- mainCode/ballisticCalculations.py
+++ mainCode/ballisticCalculations.py
@@ -1,11 +1,16 @@
-class cartridge:
+class Cartridge:
     def __init__(self, grains, velocity):
         self.grains = grains
         self.velocity = velocity 
-        kilograms = grains / 15432.09876543
-        metersPerSec = velocity / 0.3048
-        kineticEnergy = 0.5 * kilograms * metersPerSec
-        momentum = kilograms * metersPerSec
+        
+        # Conversions
+        kilograms = grains / 15432.3584                    # 1 kg ≈ 15432.36 grains
+        metersPerSec = velocity * 0.3048                 # 1 foot = 0.3048 meters
+        
+        # Formulas (Metric units)
+        self.kineticEnergyJoules = 0.5 * kilograms * (metersPerSec ** 2)
+        self.momentumKg_ms = kilograms * metersPerSec
 
-
-
+        # Calculate standard Imperial ballistics metrics
+        self.kineticEnergy_Imperial = (grains * (velocity ** 2)) / 450436  # foot-pounds (ft-lbs)
+        self.momentum_Imperial = (grains * velocity) / 225218              # pound-seconds (lbs-sec)
